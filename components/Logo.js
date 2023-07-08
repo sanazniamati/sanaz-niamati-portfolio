64240b8ac5b58159dc7dea52a7a844fae61cdfb9
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2023-07-08 19:02:40 +0330

    add framer motion

--- components/Logo.js
+++ components/Logo.js
@@ -9,7 +9,17 @@ function Logo(props) {
       <MotionLink
         href="/"
         className="w-16 h-16 bg-dark text-light rounded-full flex items-center justify-center font-bold "
-        whileHover={{ scale: 1.5 }}
+        whileHover={{
+          backgroundColor: [
+            "#121212",
+            "rgba(131,58,180,1)",
+            "rgba(253,29,29,1)",
+            "rgba(252,176,69,1)",
+            "rgba(131,58,180,1)",
+            "#121212",
+          ],
+          transition: { duration: 1, repeat: Infinity },
+        }}
       >
         CB
       </MotionLink>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,36 @@
 // library
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 // components
 import Logo from "../components/Logo";
+
+const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span
+        className={`h-[1px]  bg-dark inline-block absolute left-0 -bottom-0.5 
+      group-hover:w-full transition-[width] ease duration-300
+      ${router.asPath === href ? "w-full" : "w-0"}`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
 
 function Navbar(props) {
   return (
     <header className="w-full px-32 py-8 font-medium flex justify-between items-center">
       <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/projects">Projects</Link>
-        <Link href="/articles">Articles</Link>
+        <CustomLink href="/" title={"Home"} className="mr-4" />
+        <CustomLink href="/about" title={"About"} className="mx-4" />
+        <CustomLink href="/projects" title={"Projects"} className="mx-4" />
+        <CustomLink href="/articles" title={"Articles"} className="ml-4" />
       </nav>
-      <Logo />
+
       <nav>
         <Link href="/" target="_blank">
           T
@@ -34,6 +51,9 @@ function Navbar(props) {
           T
         </Link>
       </nav>
+      <div className=" absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
     </header>
   );
 }
