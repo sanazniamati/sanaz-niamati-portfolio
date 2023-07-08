443b704d7b09752b07d2bb66c311fd4935038254
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2023-07-08 17:00:13 +0330

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,10 @@ module.exports = {
         mont: ["var(--font-mont)", ...fontFamily.sans],
       },
       colors: {
-        dark: "",
-        light: "",
-        primary: "",
-        primaryDark: "",
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96", //240,86,199
+        primaryDark: "#58E6D9", //80,230,217
       },
     },
   },
