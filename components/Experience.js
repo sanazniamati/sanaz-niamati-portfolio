a17ff60dcc6168e5aa28349449b11dbfbbac0a4c
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2023-07-13 08:59:18 +0330

    add experince

--- components/Experience.js
+++ components/Experience.js
@@ -1,11 +1,23 @@
-import { useScroll } from "framer-motion";
+import { spring, useScroll } from "framer-motion";
 import { useRef } from "react";
 import { motion } from "framer-motion";
+// component
+import LiIcons from "../components/LiIcons";
 
 const Details = ({ position, company, companyLink, time, address, work }) => {
+  const ref = useRef(null);
   return (
-    <li className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-center">
-      <div>
+    <li
+      ref={ref}
+      className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-center"
+    >
+      <LiIcons reference="ref" />
+
+      <motion.div
+        initial={{ y: 50 }}
+        whileInView={{ y: 0 }}
+        transition={{ duration: 0.5, type: spring }}
+      >
         <h3 className=" capitalize font-bold text-2xl ">
           {position}&nbsp;
           <a href={companyLink} className=" text-primary capitalize ">
@@ -16,11 +28,11 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
           {time}| {address}
         </span>
         <p className=" font-medium w-full">{work}</p>
-      </div>
+      </motion.div>
     </li>
   );
 };
-function Experience(props) {
+function Experience() {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
     target: ref,
@@ -34,38 +46,24 @@ function Experience(props) {
       <div ref={ref} className="w-[75%] mx-auto relative ">
         <motion.div
           style={{ scaleY: scrollYProgress }}
-          className=" absolute left-8 top-0 w-[4px] h-full bg-dark origin-top"
+          className=" absolute left-9 top-0 w-[4px] h-full bg-dark origin-top"
         />
         <ul className=" w-full flex flex-col items-start justify-between ml-4 ">
           <Details
-            position="Software Engineer"
-            company="@Google"
-            companyLink={"www.google.com"}
-            time="2022-Present"
-            address="Mountain View, CA"
-            work="Worked on a team responsible for developing new features for Google's 
-          search engine, including improving the accuracy and relevance of search results and 
-          developing new tools for data analysis and visualization."
+            position="Remote - FrontEnd Developer"
+            company="Gitweld"
+            // companyLink={"www.google.com"}
+            time="June 2022-March 2023"
+            address="dusseldorf/Germany"
+            work="Drawing complex canvas graphics using React-Konva"
           />
           <Details
-            position="Software Engineer"
-            company="@Google"
-            companyLink={"www.google.com"}
-            time="2022-Present"
-            address="Mountain View, CA"
-            work="Worked on a team responsible for developing new features for Google's 
-          search engine, including improving the accuracy and relevance of search results and 
-          developing new tools for data analysis and visualization."
-          />
-          <Details
-            position="Software Engineer"
-            company="@Google"
-            companyLink={"www.google.com"}
-            time="2022-Present"
-            address="Mountain View, CA"
-            work="Worked on a team responsible for developing new features for Google's 
-          search engine, including improving the accuracy and relevance of search results and 
-          developing new tools for data analysis and visualization."
+            position="Remote - CafeTadris training course"
+            company="CafeTadris"
+            // companyLink={"www.google.com"}
+            time="November 2021-May 2023"
+            address="Tehran"
+            work="simolate Trob , Bitbarg , Terello"
           />
         </ul>
       </div>
